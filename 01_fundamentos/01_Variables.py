a75723e714822383c3beda1ade37b6a75830b1b2
Enrique_Ch <henri.ch.m.07@gmail.com>
2026-08-19 16:44:47 -0600

    feat(variables): add initial variable definitions and type demonstrations

---
+++ 01_fundamentos/01_Variables.py
@@ -0,0 +1,20 @@
+nombre = "Enrique"      #variable de tipo string
+numero = 1234567890     #variable de tipo integer
+altura = 1.80           #variable de tipo float
+x = True                #variable de tipo boolean
+
+#Concatenar variable
+print ("Hala " + nombre + " bienvedo al mundo de la programacion con python")
+
+#Casting de variable ina a str
+print("Hala " + nombre + " tu numero es: " + str(numero))
+
+#cadenas F
+print(f"Hala {nombre} tu altura es: {altura}")
+print(f"Hala {nombre} tu estado es: {x}")
+
+#Tipo de variable
+print(type(nombre))
+print(type(numero))
+print(type(altura))
+print(type(x))
