0cd42ca7ab51e112a2db25b740c3b7b45990388d
Enrique_Ch <henri.ch.m.07@gmail.com>
2026-08-21 12:58:29 -0600

    feat(diccionarios): add examples of dictionary usage and methods in 03_Diccionarios.py

---
+++ 01_fundamentos/04_Estructuras_de_datos/03_Diccionarios.py
@@ -0,0 +1,26 @@
+#uso de diccionarios #
+print("Uso de diccionarios")
+persona = {
+    "nombre": "Juan",
+    "edad": 30,
+    "ciudad": "Madrid"
+}
+print("Diccionario persona:", persona)  # Imprime {'nombre': 'Juan', 'edad': 30, 'ciudad': 'Madrid'}   
+print("Acceso a valores del diccionario:")
+print("Nombre:", persona["nombre"])  # Imprime "Juan"
+print("Edad:", persona["edad"])  # Imprime 30
+print("Ciudad:", persona["ciudad"])  # Imprime "Madrid"
+
+#Uso de metodos de diccionarios #
+print("\nUso de métodos de diccionarios")
+persona = {
+    "nombre": "Juan",
+    "edad": 30,
+    "ciudad": "Madrid"
+}
+print("Diccionario persona:", persona)  # Imprime {'nombre': 'Juan', 'edad': 30, 'ciudad': 'Madrid'}
+print(persona.keys())  # Imprime dict_keys(['nombre', 'edad', 'ciudad'])
+print(persona.values())  # Imprime dict_values(['Juan', 30, 'Madrid'])
+print(persona.items())  # Imprime dict_items([('nombre', 'Juan'), ('edad', 30), ('ciudad', 'Madrid')])
+persona.update({"profesion": "Ingeniero"})
+print("Diccionario persona actualizado:", persona)  # Imprime {'nombre': 'Juan', 'edad': 30, 'ciudad': 'Madrid', 'profesion': 'Ingeniero'}
