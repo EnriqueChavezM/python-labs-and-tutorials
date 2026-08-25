5c5c15faaf9e2339ee19ea8c5ec155ff2d1d4cc4
Enrique_Ch <henri.ch.m.07@gmail.com>
2026-08-25 14:15:54 -0600

    feat(errors): add examples of common Python errors with explanations

---
+++ 01_fundamentos/06_1.8.Errores_y_Excepciones/01_Errores.py
@@ -0,0 +1,28 @@
+"""
+#SyntaxError
+def mi_funcion() # Falta los dos puntos
+    print("Hola")
+"""
+
+"""
+#NameError
+print(variable_no_definida)
+"""
+
+"""
+#TypeError
+resultado = 5 + "10" #Operacion de tipos de variables  no compatibles
+"""
+
+"""
+#IndexError
+lista = [1, 2, 3]
+print(lista[3])  # El índice 3 está fuera del rango
+"""
+
+"""
+#IndentationError
+    edad = int(input ("ingresa tu edad: "))
+if edad >= 18:
+    print("eres mayor de edad.")
+"""
