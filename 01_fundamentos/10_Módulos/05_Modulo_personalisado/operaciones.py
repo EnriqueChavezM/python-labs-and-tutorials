5abbc79297e5e9d667d9d948aa9672d951660009
Enrique_Ch <henri.ch.m.07@gmail.com>
2026-08-27 11:31:25 -0600

    feat(modulos): add operaciones module with basic arithmetic functions and welcome message

---
+++ 01_fundamentos/10_Módulos/05_Modulo_personalisado/operaciones.py
@@ -0,0 +1,17 @@
+"""
+Crea un archivo llamado operaciones.py e inserta algunas funciones y variables:
+# Archivo: operaciones.py
+"""
+
+# Una variable del módulo
+MENSAJE_BIENVENIDA = "¡Calculadora lista para usar!"
+
+# Funciones del módulo
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def saludar(nombre):
+    return f"Hola, {nombre}, bienvenido al módulo de matemáticas."
