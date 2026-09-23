8d00599ac813345df63253885425b1cc2a1582e4
Enrique_Ch <henri.ch.m.07@gmail.com>
2026-09-23 10:48:25 -0600

    feat: add functions for updating employee information and inventory management with examples

--- 04-Retos_y_Ejercicios/02-Coddy/02_Logica_y_Flujo/02_Crear_Diccionario.py
+++ 04-Retos_y_Ejercicios/02-Coddy/02_Logica_y_Flujo/02_Crear_Diccionario.py
@@ -75,7 +75,7 @@ Crea una función llamada get_capital que toma dos parámetros:
 La función debe devolver la ciudad capital del nombre del país dado utilizando el diccionario country_capitals.
 
 *******************************************************************************************"""
-def get_capital(country_capitals, country_name):
+"""def get_capital(country_capitals, country_name):
     # Escribe el código aquí
     return country_capitals[country_name]
 
@@ -96,4 +96,89 @@ while add:
         add = False
 
 key = input("Ingrese clave a buscar\n→ ")
-print(f"El valor de la Clave es: {get_capital(diccionario,key)}")
+print(f"El valor de la Clave es: {get_capital(diccionario,key)}")"""
+
+"""******************************************************************************************
+
+Desafío 4:
+
+1. Crea una función llamada update_employee_info que reciba tres parámetros:
+    1.1. employee_dict (un diccionario) 
+    1.2. key (una cadena de texto)  
+    1.3. value. 
+2. La función debe actualizar el employee_dict con la nueva key y value. 
+3. Si la clave ya existe, su valor debe ser actualizado. 
+4. Si la clave no existe, se debe añadir un nuevo par clave-valor. 
+5. La función debe devolver el diccionario actualizado.
+
+*******************************************************************************************"""
+"""def update_employee_info(employee_dict, key, value):
+    # Escribe el código aquí
+    employee_dict[key] = value
+    return employee_dict
+
+add =  True
+conteo = 1
+diccionario = {}
+print("Crear Diccionario\n")
+while add:
+    clave = input(f"Ingrese clave {conteo}.\n→ ")
+    valor = input(f"Ingrese valor {conteo}.\n→ ")
+
+    diccionario[clave] = valor
+    print(diccionario)
+    continuar = input("Quiere agregar un elemento mas.\n¿ SI o NO?\n").upper() == "SI" 
+    if continuar:
+        conteo += 1
+    else:
+        add = False
+print(f"Diccionario creado\n{diccionario}")
+key = input("Ingrese clave a añadir/modificar\n→ ")
+value = input(f"Ingrese el valor para la  clave {key}\n→ ")
+print(f"Diccionario final: {update_employee_info(diccionario,key,value)}")"""
+
+"""******************************************************************************************
+
+Desafío 5:
+
+1. Crea una función llamada update_inventory que toma tres parámetros:
+    1.1. inventory_dict (un diccionario)
+    1.2. item (una cadena)
+    1.3. quantity (un entero). 
+2. La función debe actualizar el inventory_dict con el nuevo item y quantity. 
+3. Si el ítem ya existe en el inventario, su cantidad debe incrementarse por la cantidad dada. 
+4. Si el ítem no existe, se debe agregar un nuevo ítem con la cantidad dada. 
+5. La función debe devolver el diccionario actualizado.
+
+*******************************************************************************************"""
+def update_inventory(inventory_dict, item, quantity):
+    # Escribe el código aquí
+    if item in inventory_dict:
+        suma = int(inventory_dict[item]) + quantity
+        """print(suma)
+        print(type(suma))"""
+        inventory_dict[item] = suma
+    else:
+        inventory_dict[item] = quantity
+
+    return inventory_dict
+
+add =  True
+conteo = 1
+diccionario = {}
+print("Crear Diccionario\n")
+while add:
+    clave = input(f"Ingrese clave {conteo}.\n→ ")
+    valor = input(f"Ingrese valor {conteo}.\n→ ")
+
+    diccionario[clave] = valor
+    print(diccionario)
+    continuar = input("Quiere agregar un elemento mas.\n¿ SI o NO?\n").upper() == "SI" 
+    if continuar:
+        conteo += 1
+    else:
+        add = False
+print(f"Diccionario creado\n{diccionario}")
+key = input("Ingrese clave a añadir/modificar\n→ ")
+value = int(input(f"Ingrese el valor para la  clave {key}\n→ "))
+print(f"Diccionario final: {update_inventory(diccionario,key,value)}")
